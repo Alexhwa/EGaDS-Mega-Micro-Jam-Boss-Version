1962d117bcdd859ed28fa657819643b2c7c07cb2
loremonger <47060398+loremonger@users.noreply.github.com>
2020-12-28 15:40:26 -0600

    Rocket is hit

---
+++ Assets/Example Boss Game/Scripts/Stage 5/RocketHit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketHit : MonoBehaviour
+{
+    public Animator explode;
+    private Transform transform;
+    private float shakeDuration = 0f;
+    private float shakeMagnitude = 0.7f;
+    private float dampingSpeed = 1.0f;
+    Vector3 initialPosition;
+    
+    void Awake()
+    {
+        if (transform == null)
+        {
+            transform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable()
+    {
+        initialPosition = transform.localPosition;
+    }
+    public void RocketIsHit()
+    {
+        explode.SetTrigger("Explode");
+        shakeDuration = 0.5f;
+    }
+
+    void Update()
+    {
+
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+
+            shakeDuration -= Time.deltaTime * dampingSpeed;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = initialPosition;
+        }
+    }
+}
