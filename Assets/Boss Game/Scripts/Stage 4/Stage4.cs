45db14c7d5d28680de8c9a754cae2d263258c8b9
Alexhwa <ah.alswad@gmail.com>
2020-12-28 18:09:29 -0600

    Stage 4 physics

---
+++ Assets/Boss Game/Scripts/Stage 4/Stage4.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeeNice
+{
+    public class Stage4 : StageController
+    {
+        public static StageController instance;
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+            instance = this;
+        }
+
+    }
+}
